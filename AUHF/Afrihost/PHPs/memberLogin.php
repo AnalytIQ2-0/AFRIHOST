27d88f8e572f720ed35bdf048624dbc4bafa038a
boitumelo masesi <1604352@students.wits.ac.za>
2020-08-19 09:29:45 +0200

    "getting started with elaboration 3"

---
+++ AUHF/Afrihost/PHPs/memberLogin.php
@@ -0,0 +1,33 @@
+<?php
+    $username = "root";
+    $database = "afrihost";
+    $password = "";
+    $conn = mysqli_connect("127.0.0.1", $username, $password, $database);
+
+    $email=$_REQUEST[''];
+
+    $query = "SELECT * FROM member";
+    $output=array();
+    if($conn){
+        if($result = mysqli_query($conn,$query)){
+            while($row=$result->fetch_assoc()){
+                $output[]=$row;
+            }
+        }
+        else{
+          $arr = array(array("member_id" =>"false"));
+          echo json_encode($arr);
+          die(mysqli_error($conn));
+        }
+    }else{
+        $arr= array(array("member_id"=>"false"));
+    }
+    mysqli_close($conn);
+    if(empty($output)){
+        $arr= array(array("member_id"=>"false"));
+        echo json_encode($arr);
+    }else{
+      echo json_encode($output);
+    }
+    die();
+?>
